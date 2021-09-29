7e2e498d363434f4e9707b99fdd4c01921183e82
Dylan King <dylan.a.king@outlook.com>
2021-09-29 16:10:52 -0700

    add boilerplate

--- bot.py
+++ bot.py
@@ -1,1 +1,28 @@
+import os
 
+import discord
+from dotenv import load_dotenv
+
+
+def get_token() -> str:
+    """ Get the token from the ".env" file. """
+    load_dotenv()
+    DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+    assert DISCORD_TOKEN is not None, "Unable to find discord token in \".env\"."
+    return DISCORD_TOKEN
+
+
+TOKEN = get_token()
+
+
+class Bot_Client(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        await message.channel.send("hello")
+        pass
+
+
+client = Bot_Client()
+client.run(TOKEN)
